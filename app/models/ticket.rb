d7d744280f1d9b59f8607aebc5ab14fa8f44db73
Abhinav Mathur <amathur5000@gmail.com>
2015-08-31 17:21:47 +0000

    Cull tickets when project gets destroyed

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,5 @@
 class Ticket < ActiveRecord::Base
-  belongs_to :project
+  belongs_to :project, dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true, length: {minimum: 10}
   
