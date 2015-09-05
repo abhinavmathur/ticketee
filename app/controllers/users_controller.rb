7fdfc4ed20c21430413eef48d46e3985a07b7623
Abhinav Mathur <amathur5000@gmail.com>
2015-09-05 11:37:41 +0000

    FIX

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+<<<<<<< HEAD
   before_action :define_user
+=======
+  before_action :set_user
+  
+>>>>>>> signing-up
   
   def new
     @user = User.new
@@ -24,6 +29,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       flash[:notice] = "Profile has been updated."
+<<<<<<< HEAD
       redirect_to user_path
     else
       flash[:alert] = "Profile was not updated"
@@ -31,6 +37,15 @@ class UsersController < ApplicationController
     end
   end
 
+=======
+      redirect_to @user
+    else
+      flash[:error] = "Profile was not updated"
+      render :edit
+    end
+  end
+  
+>>>>>>> signing-up
   private
   
   def user_params
@@ -38,6 +53,10 @@ class UsersController < ApplicationController
   end
   
   def define_user
+    @user = User.find(params[:id])
+  end
+  
+  def set_user
     @user = User.find(params[:id])
   end
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,8 +1,15 @@
 FactoryGirl.define do
     factory :user do
+<<<<<<< HEAD
         name "Abhinav Mathur"
         email "raaaaj5000@yahoo.co.in"
         password "hello"
         password_confirmation "hello"
+=======
+        name "Raj"
+        email "r@a.com"
+        password "0522Lucknow"
+        password_confirmation "0522Lucknow"
+>>>>>>> signing-up
     end
 end
