adfedc300eb784cd520ee5af0e5503708baa5de5
Abhinav Mathur <amathur5000@gmail.com>
2015-09-05 11:29:24 +0000

    Completed Signing UP for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user
+  
+  
   def new
     @user = User.new
   end
@@ -16,9 +19,26 @@ class UsersController < ApplicationController
   def show
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Profile has been updated."
+      redirect_to @user
+    else
+      flash[:error] = "Profile was not updated"
+      render :edit
+    end
+  end
+  
   private
   
   def user_params
     params.require(:user).permit(:name,:password,:password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name "Raj"
+        email "r@a.com"
+        password "0522Lucknow"
+        password_confirmation "0522Lucknow"
+    end
+end
